7a8986efca66aaf9d36c433886c8acdf951cd223
ndbzika <chakcpps@gmail.com>
2023-09-29 23:04:42 -0300

    fix: Corrigindo e melhorando serviços

--- src/services/technologyService.ts
+++ src/services/technologyService.ts
@@ -1,8 +1,24 @@
 import { db } from "../database/db";
 import { TechnologyDTO } from "../dtos/technologiesDTO";
+import { userService } from "./userService";
+
+const TechnologyExists = (id: string, username: string) => {
+  const user = userService.UserExists(username);
+  if (!user) {
+    return false;
+  }
+  const technology = findTechnology(id, username);
+  if (!technology) {
+    return false;
+  }
+  return true;
+};
 
 const findTechnology = (id: string, username: string) => {
-  const user = db.find((user) => user.username === username);
+  if (!TechnologyExists(id, username)) {
+    return false;
+  }
+  const user = userService.findUserByUsername(username);
   const technology = db.find(() =>
     user?.technologies.find((technology: TechnologyDTO) => technology.id === id)
   );
@@ -10,32 +26,71 @@ const findTechnology = (id: string, username: string) => {
 };
 
 const findAllTechnologies = (username: string) => {
-  const user = db.find((user) => user.username === username);
+  if (!userService.UserExists(username)) {
+    return false;
+  }
+  const user = userService.findUserByUsername(username);
   return user?.technologies;
 };
 
 const createTechnology = (username: string, technology: TechnologyDTO) => {
-  const user = db.find((user) => user.username === username);
+  if (!userService.UserExists(username)) {
+    return false;
+  }
+  const user = userService.findUserByUsername(username);
   user?.technologies.push(technology);
 };
 
-const updateTechnology = (username: string, id: string ,title: string, deadline:Date) => {
-  const technology = findTechnology(id, username);
-  technology.title = title || technology.title;
-  technology.deadline = new Date(deadline) || technology.deadline;
-}
+const updateTechnology = (
+  username: string,
+  id: string,
+  title: string,
+  deadline: Date
+) => {
+  if (!userService.UserExists(username)) {
+    return false;
+  }
+  const user = userService.findUserByUsername(username);
+  if (!TechnologyExists(id, username)) {
+    return false;
+  }
+  const tech = user?.technologies.map((tech) => {
+    if (tech.id === id) {
+      tech.title = title || tech.title;
+      tech.deadline = deadline || tech.deadline;
+    }
+  });
+  return tech;
+};
 
 const updateTechnologyStatus = (username: string, id: string) => {
-  const technology = findTechnology(id, username);
-  technology.studied = true;
-}
+  if (!userService.UserExists(username)) {
+    return false;
+  }
+  const user = userService.findUserByUsername(username);
+  if (!TechnologyExists(id, username)) {
+    return false;
+  }
+  user?.technologies.map((tech: TechnologyDTO) => {
+    if (tech.id === id) {
+      tech.studied = true;
+    }
+  });
+  return true;
+};
 
 const deleteTechnology = (username: string, id: string) => {
+  if (!userService.UserExists(username)) {
+    return false;
+  }
+  const user = userService.findUserByUsername(username);
+  if (!TechnologyExists(id, username)) {
+    return false;
+  }
   const technology = findTechnology(id, username);
-  const user = db.find((user) => user.username === username);
-  const index = user?.technologies.indexOf(technology);
+  const index = user?.technologies.indexOf(technology as TechnologyDTO);
   user?.technologies.splice(index!, 1);
-}
+};
 
 export const technologyService = {
   findTechnology,
@@ -43,5 +98,5 @@ export const technologyService = {
   createTechnology,
   updateTechnology,
   updateTechnologyStatus,
-  deleteTechnology
+  deleteTechnology,
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -17,21 +17,36 @@ const findAllUsers = () => {
   return db;
 };
 
+const UserExists = (username: string) => {
+  const user = findUserByUsername(username);
+  if (user) {
+    return true;
+  }
+  return false;
+};
+
 const createUser = (user: UserDTO) => {
   db.push(user);
+  return true;
 };
 
 const updateUser = (id: string, user: UserDTO) => {
+  if (!UserExists(user.username)) {
+    return false;
+  }
   const index = db.findIndex((currentId) => currentId.id === id);
   db[index] = user;
+  return true;
 };
 
 const deleteUser = (id: string) => {
   const index = db.findIndex((currentId) => currentId.id === id);
   db.splice(index, 1);
+  return true;
 };
 
 export const userService = {
+  UserExists,
   findUserById,
   findUserByUsername,
   findAllUsers,
